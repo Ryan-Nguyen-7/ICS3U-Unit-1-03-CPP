f8832004d62cd67c7c3e5e66a8347f43d9a72773
Ubuntu <ubuntu@ip-172-31-40-214.ec2.internal>
2020-11-25 18:03:27 +0000

    added helloWorld.cpp program, proper format

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+// Copyright (C) 2020 St. Mother Teresa HS All rights reserved
+//
+// created by Ryan Nguyen
+// Created on November 2020
+// This is the "Hello, World!" program, in proper format, CPP
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
